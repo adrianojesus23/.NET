7510c908a7184245cd55800596b7c0e7709fa189
adrianovaz23 <adrianovaz23@hotmail.com>
2022-10-15 18:21:15 +0100

    Add class

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number = 420;
+
+unsafe
+{
+    var ptr = stackalloc int[1];
+    int* pointer = &number;
+}
+
+var app = new Application();
+DateStruct dateStruct = new DateStruct() { Day = 2, Month = 2, Year = 2};
+
+Console.WriteLine(number);
+
+public class Application
+{
+    private int number = 66;
+}
+
+public struct DateStruct
+{
+    public int Day { get; set; }
+    public int Month { get; set; }
+    public int Year { get; set; }
+}
