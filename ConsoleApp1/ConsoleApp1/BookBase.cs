f7f8c1c8674ff157372235cdf5fd725d5f081d1b
adrianovaz23 <adrianovaz23@hotmail.com>
2022-10-16 10:01:39 +0100

    Add 104

--- ConsoleApp1/ConsoleApp1/BookBase.cs
+++ ConsoleApp1/ConsoleApp1/BookBase.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age10 { get; set; }
+        public int Age102 { get; set; }
+        public int Age104 { get; set; }
     }
 }
