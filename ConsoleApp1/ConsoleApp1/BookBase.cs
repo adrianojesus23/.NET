53db2c45d5718ab3e12384da29802e39772798af
adrianovaz23 <adrianovaz23@hotmail.com>
2022-10-16 10:03:22 +0100

    Change

--- ConsoleApp1/ConsoleApp1/BookBase.cs
+++ ConsoleApp1/ConsoleApp1/BookBase.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public int Age10 { get; set; }
         public int Age102 { get; set; }
-        public int Age104 { get; set; }
+        public int Age105 { get; set; }
+        public int Age106 { get; set; }
     }
 }
